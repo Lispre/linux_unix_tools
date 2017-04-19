df7065b069067922b56e4c20125d14d471d776c2
lampo.wei <lampo.wei@fuwo.com>
2017-04-19 11:34:34 +0800

    Add Cancel thread resource safe free support

--- process_thread/CancelThreadFromOutsideTheProcess.cpp
+++ process_thread/CancelThreadFromOutsideTheProcess.cpp
@@ -2,13 +2,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <errno.h>
 
 /**
  * 结束从线程外部结束一个线程的正确范式是通过pthread_cancel激活线程的内部的取消点
  */
 
+int footprint = 0;
+char* storage = nullptr;
+
+void freerc(void* s)
+{
+	free(s);
+	puts("the free called");
+}
+
+static void checkResults(char* string, int rc)
+{
+	if (rc)
+	{
+		printf("Error on: %s, rc = %d", string, rc);
+		exit(EXIT_FAILURE);
+	}
+	
+	return;
+}
+
 void* threadfunc(void* parm)
 {
+	int rc = 0;
+	int oldState = 0;
+
+	rc = pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, &oldState);
+	checkResults("pthread_setcancelstate()\n", rc);
+
+	if ((storage = (char*) malloc(80)) == nullptr)
+	{
+		perror("malloc() failed!");
+		exit(6);
+	}
+
+	rc = pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, &oldState);
+	checkResults("pthread_setcancelstate()\n", rc);
+
+	//注册非正常退出的资源清理函数
+	pthread_cleanup_push(freerc, storage);
+
+	puts("thread has obtained storage and is waiting to be cancneled");
+	footprint++;
   	printf("Entered secondary thread\n");
   	while (1) 
   	{
@@ -19,35 +60,47 @@ void* threadfunc(void* parm)
 
     	sleep(1);
   	}
-  	return NULL;
+  	pthread_cleanup_pop(1);
+
+  	return nullptr;
 }
  
 int main(int argc, char **argv)
 {
-  	pthread_t             thread;
-  	int                   rc=0;
- 
-  	printf("Entering testcase\n");
- 
-  	/* Create a thread using default attributes */
-  	printf("Create thread using the NULL attributes\n");
-  	rc = pthread_create(&thread, NULL, threadfunc, NULL);
-  	printf("pthread_create(NULL) %d\n", rc);
- 
-	/* sleep() is not a very robust way to wait for the thread */
-	sleep(1);
+  	pthread_t tid;
+  	void* status = nullptr;
+  	if (pthread_create(&tid, nullptr, threadfunc, nullptr) != 0)
+  	{
+  		perror("pthread_create() error");
+  		exit(1);
+  	}
 
-	printf("Cancel the thread\n");
-	
-	
-	//通知线程取消
-	rc = pthread_cancel(thread);
-	
-	
-	printf("pthread_cancel() %d\n", rc);
+  	while (footprint == 0) 
+  	{
+  		sleep(1);
+  	}
 
-	/* sleep() is not a very robust way to wait for the thread */
-	sleep(10);
-	printf("Main completed\n");
-	return 0;
+  	puts("Starting cancelling thread");
+  	if (pthread_cancel(tid) != 0)
+  	{
+  		perror("pthread_cancel() error");
+  		sleep(2);
+  		exit(3);
+  	}
+
+  	if (pthread_join(tid, &status) != 0)
+  	{
+  		if (status != PTHREAD_CANCELED)
+  		{
+  			perror("pthread_join() error");
+  			exit(4);
+  		}
+  		if (status == PTHREAD_CANCELED)
+  		{
+  			puts("PTHREAD_CANCELED");
+  		}
+  		puts("main exit");
+  	}
+
+  	return 0;
 }
